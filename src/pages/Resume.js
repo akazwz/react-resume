2e22af7c37adf93bdc4c2a14f5b7227034c18b7e
akazwz <akazwz@pm.me>
2021-08-16 23:04:31 +0800

    material

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,10 @@
+import Button from '@material-ui/core/Button';
+
 const Resume = () => {
     return (
         <div>
             this is resume.
+            <Button>btn</Button>
         </div>
     );
 }
